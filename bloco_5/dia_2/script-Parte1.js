aa6c3e32de4553ef02ba6851dcd289193bf227c5
thiagoalmeida22 <thiagoguimaraesalmeida@gmail.com>
2021-07-03 19:59:53 -0300

    Parte II - Feita

--- bloco_5/dia_2/script-Parte1.js
+++ bloco_5/dia_2/script-Parte1.js
@@ -21,3 +21,26 @@ let task7 = task1.nextElementSibling;
 
 //8
 let task8 = document.getElementById('pai').children[2];
+
+// -=-=-=-=-=-=-=-=- Fim Parte 1
+
+// -=-=-=-=-=-=-=-=- Começo Parte 2
+
+//1
+let brotherDiv = document.createElement('div');
+// brotherDiv.innerText = "testão" So pra eu saber que ta sendo criado mesmo, diferençiando a nova div.
+// brotherDiv.id = "lek"
+task1.parentNode.appendChild(brotherDiv);
+
+//2
+let childDiv = document.createElement('div');
+task1.appendChild(childDiv);
+
+//3
+let childDiv2 = document.createElement('div');
+document.getElementById('primeiroFilhoDoFilho').appendChild(childDiv2);
+
+
+//4
+document.getElementById('primeiroFilhoDoFilho').children[0].parentNode.parentNode.parentNode.children[2];
+// Chego no div de ID terceiroFilho
