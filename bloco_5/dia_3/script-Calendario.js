590b5b880d8a75eb3bf4a2eb1a738df833c12d80
thiagoalmeida22 <thiagoguimaraesalmeida@gmail.com>
2021-07-05 18:49:15 -0300

    Exercicio 1 Feito

---
+++ bloco_5/dia_3/script-Calendario.js
@@ -0,0 +1,38 @@
+function createDaysOfTheWeek() {
+    const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+    const weekDaysList = document.querySelector('.week-days');
+  
+    for (let index = 0; index < weekDays.length; index += 1) {
+      const days = weekDays[index];
+      const dayListItem = document.createElement('li');
+      dayListItem.innerHTML = days;
+  
+      weekDaysList.appendChild(dayListItem);
+    };
+  };
+  
+createDaysOfTheWeek();
+  
+//Ex: <li class="day">3</li>
+// Escreva seu código abaixo.
+
+function createdezDays() {
+    const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];;
+    const daysFather = document.getElementById('days');
+
+    for (let index = 0; index < dezDaysList.length; index += 1) {
+        const dezDayListItem = document.createElement('li');
+        dezDayListItem.innerHTML = dezDaysList[index];
+        dezDayListItem.classList.add('day');
+        if (dezDaysList[index] === 24 || dezDaysList[index] === 25 || dezDaysList[index] === 31) {
+            dezDayListItem.classList.add('holiday');
+        }
+        if (dezDaysList[index] === 4 || dezDaysList[index] === 11 || dezDaysList[index] === 18 || dezDaysList[index] === 25) {
+            dezDayListItem.classList.add('friday');
+        }
+
+        daysFather.appendChild(dezDayListItem);  
+    }
+}
+
+createdezDays();
